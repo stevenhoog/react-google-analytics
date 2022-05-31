c9fe00aff93d7c2c882f90d4a1dd81be695335ac
Steven Hoogervorst <s.hoogervorst@hotmail.com>
2022-05-31 07:54:41 +0200

    Connect app with Analytics view

--- src/App.js
+++ src/App.js
@@ -1,25 +1,61 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState, useEffect} from "react";
+import ReactEcharts from "echarts-for-react";
+import "./App.css";
+import { renderButton, checkSignedIn } from "./utils";
 
-function App() {
+function App() {  
+  const option = {
+    tooltip: {
+        trigger: "item",
+        formatter: "{b} : {c}"
+      },
+      xAxis: {
+        type: 'category',
+        data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
+        },
+      yAxis: {
+        type: 'value'
+      },
+      series: [
+        {
+          data: [150, 230, 224, 218, 135, 147, 260],
+          type: 'line'
+        }
+      ]
+  }; 
+
+  const [isSignedIn, setIsSignedIn] = useState(false);
+
+  const updateSignin = (signedIn) => { //(3)
+    setIsSignedIn(signedIn);
+    if (!signedIn) {
+      renderButton();
+    }
+  };
+
+  const init = () => { //(2)
+    checkSignedIn()
+      .then((signedIn) => {
+        updateSignin(signedIn);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  };
+
+  useEffect(() => {
+    window.gapi.load("auth2", init); //(1)
+  });
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {!isSignedIn ? (
+        <div id="signin-button"></div>
+      ) : (
+        <ReactEcharts option={option} style={{ height: 300 }} />
+      )}
     </div>
   );
-}
+} 
 
 export default App;
